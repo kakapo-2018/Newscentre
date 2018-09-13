af7e6d18ed55412574d72adb23deb963a36930b5
leslie-alldridge <leslie.alldridge@gmail.com>
2018-09-14 11:26:00 +1200

    form done email works

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Words from './Words'
 import AddWord from './AddWord'
+import Form from './Form'
 
 const App = () => (
   <div className='app-container'>
     <Words />
     <AddWord />
+    <Form />
   </div>
 )
 
