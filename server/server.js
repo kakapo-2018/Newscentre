32744d9abf5c68d50616bdedb6ab3fc0de2776ba
mattritani <mattritani@gmail.com>
2018-09-14 11:20:19 +1200

    rightwingsourcesadded

--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
 const path = require('path')
 const express = require('express')
+const request = require('superagent')
 
+const rightSources = 'breitbart-news,fox-news'
+
+const apiKey = 'apiKey=324d17864acd4c8387e3999687de6f1a'
 const server = express()
+const apiEndPoint = `https://newsapi.org/v2/everything?`
+//const foxNews = 'sources=al-jazeera-english'
+
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, '../public')))
+
+server.get('/search/trump', (req,res) => {
+  const query = 'Breitbart'
+  request.get(apiEndPoint+'sources='+rightSources+'&'+'q='+query+'&'+apiKey)
+    .then (ApiRes => {
+      res.json(ApiRes.body)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 module.exports = server
