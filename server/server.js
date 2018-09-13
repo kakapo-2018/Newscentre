0bf9bfbdd4cf58d5e713e05a06dfb87881d5e6d1
mattritani <mattritani@gmail.com>
2018-09-14 11:42:54 +1200

    working left and right sides

--- server/server.js
+++ server/server.js
@@ -2,26 +2,37 @@ const path = require('path')
 const express = require('express')
 const request = require('superagent')
 
-const rightSources = 'breitbart-news,fox-news'
+const rightSources = 'breitbart-news,fox-news,fox-sports'
+const leftSources = 'liberation'
 
 const apiKey = 'apiKey=324d17864acd4c8387e3999687de6f1a'
 const server = express()
 const apiEndPoint = `https://newsapi.org/v2/everything?`
-//const foxNews = 'sources=al-jazeera-english'
+
 
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
-server.get('/search/trump', (req,res) => {
-  const query = 'Breitbart'
-  request.get(apiEndPoint+'sources='+rightSources+'&'+'q='+query+'&'+apiKey)
-    .then (ApiRes => {
-      res.json(ApiRes.body)
+
+
+
+server.get('/search',(req,res) => {
+  const query = 'trump'
+  Promise.all([
+    request.get(apiEndPoint+'sources='+rightSources+'&'+'q='+query+'&'+apiKey),
+    request.get(apiEndPoint+'sources='+leftSources+'&'+'q='+query+'&'+apiKey)
+  ]).then(resultsArray => {
+    res.json({
+      left: resultsArray[0].body,
+      right: resultsArray[1].body
     })
     .catch(err => {
       console.log(err)
     })
+  })
 })
+
+
 
 module.exports = server
